890d9849589f4de91597a28a4d6993922924879a
Albert <walickialbert@gmail.com>
2022-11-01 13:52:23 +0100

    added more options  - styling + rn  + external style file

--- src/plopfile.js
+++ src/plopfile.js
@@ -1,6 +1,17 @@
+/* eslint-disable no-console */
 const fs = require('fs');
 const shell = require('shelljs');
 const setHelpers = require('./setHelpers');
+
+const EXTENSION_TSX = 'tsx';
+const EXTENSION_JS = 'js';
+const PROJECT_TYPE_REACT = 'react';
+const PROJECT_TYPE_REACT_NATIVE = 'react-native';
+const STYLING_MODULE_SCSS = 'module.scss';
+const STYLING_EMOTION = 'emotion';
+const STYLING_STYLED_COMPONENTS = 'styled-components';
+const EXTERNAL_STYLES = 'external';
+const INTERNAL_STYLES = 'internal';
 
 const CONFIG_PATH = './.retooly.json';
 
@@ -43,27 +54,55 @@ module.exports = function plopMain(plop) {
         message: 'Provide component name',
       },
     ],
-    actions: [
-      {
+    actions() {
+      const basePath = `${currentPath}/{{name}}`;
+      const actions = [];
+
+      let projectType = 'react';
+
+      if (projectType === PROJECT_TYPE_REACT_NATIVE) {
+        projectType = 'react-native';
+      }
+
+      if (projectType === PROJECT_TYPE_REACT) {
+        if (config?.projectType === STYLING_MODULE_SCSS) {
+          projectType = 'react-modules';
+        } else {
+          projectType = 'react';
+        }
+      }
+
+      actions.push({
         type: 'add',
-        path: `${currentPath}/{{name}}/index.${
-          config?.projectExtension || 'js'
-        }`,
+        path: `${basePath}/index.${config?.extension || 'js'}`,
         templateFile: 'templates/index.hbs',
-      },
-      {
+      });
+
+      actions.push({
         type: 'add',
-        path: `${currentPath}/{{name}}/{{name}}.${
-          config?.projectExtension || 'js'
-        }`,
-        templateFile: `templates/component.hbs`,
-      },
-      {
-        type: 'add',
-        path: `${currentPath}/{{name}}/{{name}}-style.module.scss`,
-        templateFile: `templates/style.hbs`,
-      },
-      function lint(answers) {
+        path: `${basePath}/{{name}}.${config?.extension || 'js'}`,
+        templateFile: `templates/${projectType}.hbs`,
+      });
+
+      if (config?.externalStyles === 'external' && config?.styling) {
+        let ext;
+        switch (config.styling) {
+          case STYLING_EMOTION:
+          case STYLING_STYLED_COMPONENTS:
+            ext = config.extension;
+            break;
+          default:
+            ext = config.styling;
+        }
+
+        actions.push({
+          type: 'add',
+          path: `${basePath}/{{name}}-style.${ext}`,
+          templateFile: `templates/style.hbs`,
+        });
+      }
+
+      actions.push((answers) => {
         if (
           packageJson?.dependencies?.eslint ||
           packageJson?.devDependencies?.eslint
@@ -76,19 +115,46 @@ module.exports = function plopMain(plop) {
             console.log("Couldn't lint the code");
           }
         }
-      },
-    ],
+      });
+
+      return actions;
+    },
   });
 
-  plop.setGenerator('Set project type (TSX/JS)', {
+  plop.setGenerator('Set project config', {
     description: 'Configures project type',
     prompts: [
       {
         type: 'list',
-        name: 'projectExtension',
+        name: 'extension',
+        message: 'Select extension type',
+        choices: [EXTENSION_TSX, EXTENSION_JS],
+        default: config?.extension ?? [],
+      },
+      {
+        type: 'list',
+        name: 'projectType',
         message: 'Select project type',
-        choices: ['tsx', 'js'],
+        choices: [PROJECT_TYPE_REACT, PROJECT_TYPE_REACT_NATIVE],
         default: config?.projectType ?? [],
+      },
+      {
+        type: 'list',
+        name: 'styling',
+        message: 'Select prefered styling method',
+        choices: [
+          STYLING_EMOTION,
+          STYLING_MODULE_SCSS,
+          STYLING_STYLED_COMPONENTS,
+        ],
+        default: config?.styling ?? [],
+      },
+      {
+        type: 'list',
+        name: 'externalStyles',
+        message: 'Select if styles should be in an external file',
+        choices: [EXTERNAL_STYLES, INTERNAL_STYLES],
+        default: config?.externalStyles ?? [],
       },
     ],
     actions: [
